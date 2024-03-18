e597d178bd1d3888c28468d33879aa568d23d345
Dr milli <100302736+drmilli@users.noreply.github.com>
2024-03-18 23:52:15 +0100

    remove old admin page

--- admin_old/layouts/top-nav.php
+++
@@ -1,8 +0,0 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
-    <a class="navbar-brand" href="index.php">Sabi ride Blog Panel</a>
-    <section class="collapse navbar-collapse" id="navbarSupportedContent"></section>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="text-decoration-none text-white" href="../../sabi_ride/auth/logout.php">logout</a>
-</nav>
