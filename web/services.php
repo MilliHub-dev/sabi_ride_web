6d43ba454b03efb8368c2269a515a8b5df7ddd88
Dr milli <ekeminieffiong115@gmail.com>
2024-06-14 08:00:45 -0700

    php

--- web/services.php
+++ web/services.php
@@ -12,7 +12,7 @@
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" i href="index.php"> <img src="../sabimages/Sabi ride 2.png" width="100px" alt=""></a>
+    <a class="navbar-brand" i href="../index.php"> <img src="../sabimages/Sabi ride 2.png" width="100px" alt=""></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
