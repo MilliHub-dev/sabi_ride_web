3935ec765556b20c5e7ff38667f5d9798879371f
Asere Oluwatumise S <tumixs.dev@outlook.com>
2024-06-25 02:20:12 +0100

    update: New styles on alernate branch

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sabi Ride</title>
   <link rel="icon" type="image/x-icon" href="./sabimages/Sabiride3.png">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="./style/loader.css">
-  <!-- <link rel="stylesheet" href="./style/style1.css"> -->
-  <link rel="stylesheet" href="./css/fonts.css">
+  <!-- <link rel="stylesheet" href="./style/loader.css"> -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="./style/verification.css">
   <link rel="stylesheet" href="./style/main.css">
+  <link rel="stylesheet" href="./style/boostrap_overrides.css">
 
 
 </head>
@@ -27,168 +24,146 @@
   </div> -->
 
 <body>
-  <!-- NAVBAR -->
-  <nav class=" navbar sticky-top navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" i href="index.php"> <img src="./sabimages/Sabi ride 2.png" width="100px" alt=""></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="web/driver.php">Drive or deliver</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Ride</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="web/blog2.php">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="web/about.php">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="web/services.php">Services</a>
-          </li>
-        </ul>
-        <div class="d-flex me-4 mb-1">
-          <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-globe me-1" style="font-size: 1.3rem; color: rgb(0, 0, 0);"></i>EN</a>
-        </div>
-        <div class="d-flex">
-          <ul class="nav justify-content-end">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="">Help</a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="web/sabi_login.php">Login</a>
-            </li>
-
-          </ul>
-          <button style="background-color: #0088CD; outline: none; border: 0; border-radius: 20px; height: 30px; padding: 0 10px; margin-top: 5px;"><a href="web/driver.php" style="text-decoration: none; color: #fff;">Sign up</a></button>
-
-
-
-
-
-
-
-        </div>
-      </div>
-    </div>
-  </nav>
-
 
   <!-- Page Hero -->
-  <div class="parallax bgimg-1 pt-5">
-    <div class="container-fluid">
-      <div class="row g-0 justify-content-between min-vh-100">
-        <div>
-          <div class="row gx-2 justify-content-between">
-            <div class="col-12 gx-2">
-              <div class="text-center">
-                <img src="./sabimages/home11.png" alt="SabiRide App interface" id="phonemockup">
-              </div>
-              <div class="brandcaption">
-                <span class="text-start text-white text-wrap">Anywhere you dey go,<br>we <span class="bluey">sabi</span> am.</span>
-              </div>
+  <div class="bgimg-1 parallax">
+    <!-- NAVBAR -->
+    <nav class=" navbar navbar-expand-lg ps-4 ps-md-5 mt-0 pt-0">
+      <div class="container-fluid">
+        <!-- Mobile NAV Toggle -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- brand-logo -->
+        <a class="navbar-brand me-5" href="index.php"> <img src="./sabimages/Sabi ride 2.png" alt="Logo" width="150px" class="d-inline-block align-text-top text-white">
+        </a>
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <img class="offcanvas-title" id="offcanvasNavbarLabel" src="./sabimages/Sabi ride 2.png" alt="Logo" width="150px" class="d-inline-block align-text-top text-white"></img>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-start flex-grow-1 justify-content-end pe-3">
+              <li class="nav-item">
+                <a class="nav-link active " href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="web/driver.php">Drive or deliver</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="#">Ride</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="web/blog2.php">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="web/about.php">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="web/services.php">Services</a>
+              </li>
+            </ul>
 
+
+          </div>
+        </div>
+    </nav>
+    <!-- Hero -->
+    <div class="container-fluid ps-4 ps-md-5 mt-0">
+      <div class="row g-2 justify-content-start mt-0">
+        <div class="col-8">
+          <div class="brandcaption">
+            <span class="text-start text-white">Anywhere you dey go,<br>we <span class="bluey">sabi</span> am.</span>
+          </div>
+        </div>
+        <div class="col-10 justify-content-start mt-5">
+          <div class="lead-caption">
+            <div class="text-start text-white text-wrap text-justify"> Sabi Ride is a social ride hailing/sharing platform revolved around providing convenience, safety, and efficiency for both riders and drivers. Sabi ride is driven by safety, fueled by convenience.
             </div>
           </div>
         </div>
-        <div class="col-12 mt-5">
-          <div class="caption0">
-            <div class="text-start fw-normal text-white mb-3 lead"> Sabi Ride is a social ride hailing/sharing platform revolved around providing convenience, safety, and efficiency for both riders and drivers. Sabi ride is driven by safety, fueled by convenience.
-            </div>
-          </div>
-
-        </div>
-        <div class=" col-12">
-          <div class=" caption1 " id="btn">
-            <button class="btn btn-link " type="button" style="height: 60px; margin-bottom: 10px;">Take a ride With Sabi Ride</button>
-            <button class="btn btn-link" type="button" style="height: 60px; margin-bottom: 10px;">Drive with Sabi Ride</button>
-          </div>
+        <div class="d-flex  mb-5 gap-3 py-3 flex-row flex-wrap">
+          <button class="btn btn-sm btn-light d-flex flex-row align-items-center justify-content-center justify-content-between text-nowrap" type="button">Become a Driver <i class="bi bi-chevron-right style=" font-size: 2rem;" aria-hidden="true"></i></button>
+          <span class="me-md-auto"></span>
+          <button class="btn btn-sm btn-dark text-nowrap d-flex flex-row justify-content-between text-white" type="button"><i class="bi bi-apple" style="font-size: 2rem;" aria-hidden=" true"></i><span class="d-flex flex-column pe-2"><small>Download on the</small> App Store</span></button>
+          <button class="btn btn-sm btn-dark text-nowrap  d-flex flex-row justify-content-between text-white" type="button"><i class="bi bi-play" style="font-size: 2rem;" aria-hidden=" true"></i><span class="d-flex flex-column justify-content-between pe-2"><small>Download on the</small> Play Store</span></button>
         </div>
       </div>
+
     </div>
   </div>
+
 
   <!-- Service Heading -->
-  <div class="container-fluid row mt-4">
+  <div class="container-fluid mt-4 ps-4 ps-md-5 ">
     <div class="row justify-content-center">
-      <div class="col-12">
-        <h3 class="text-start display-3 fw-bold" style="padding-left:6%;  text-align: justify;">Our services</h3>
+      <div class="col-12 g-2 ">
+        <h3 class="text-start display-4 fw-bold">Our services</h3>
+        <p>Our In-app services stretch out to serve other transportation needs.</p>
       </div>
     </div>
-  </div>
+    <!-- Serivce cards -->
+    <div class="mt-2">
+      <div class="container-fluid services">
+        <div class="row gy-2 justify-content-between">
+          <div class=" col-6 col-md-4">
+            <div class="card h-100">
+              <div class="card-body">
+                <h5 class="card-title">Rides</h5>
+                <p class="card-text float-start"> Set location, offer price start trip </p>
+                <img src="./img/rides.png" class="float-end" alt="Rides">
+              </div>
+            </div>
+          </div>
+          <div class="col-6 col-md-4">
+            <div class="card h-100">
+              <div class="card-body">
+                <h5 class="card-title">Dispatch</h5>
+                <p class="card-text float-start">Swift order delivery with our riders. </p>
+                <img src="./img/dispatch.png" class="float-end" width="120px" alt="Dispatch">
+              </div>
+            </div>
+          </div>
+          <div class="col-6 col-md-4">
+            <div class="card h-100">
+              <div class="card-body">
+                <h5 class="card-title">Group Rides</h5>
+                <p class="card-text float-start">Coordinate group rides with friends and family</p>
+                <img src="./img/group_ride.png" class="float-end" alt="Group Rides">
+              </div>
+            </div>
+          </div>
+          <div class="col-6 col-md-4">
+            <div class="card h-100">
+              <div class="card-body">
+                <h5 class="card-title">VIP Rides</h5>
+                <p class="card-text float-start "> You prioritize comfort and your luxury, </p>
+                <img src="./img/vip_ride.png" class="float-end" alt="VIP">
+              </div>
+            </div>
+          </div>
+          <div class="col-6 col-md-4">
+            <div class="card h-100">
+              <div class="card-body">
+                <h5 class="card-title">Logistics</h5>
+                <p class="card-text float-start">We cover logistics for SMEs.</p>
+                <img src="./img/logistics.png" class="float-end" alt="Logistics">
+              </div>
+            </div>
+          </div>
+          <div class="col-6 col-md-4">
+            <div class="card h-100">
+              <div class="card-body">
+                <h5 class="card-title">Groceries</h5>
+                <p class="card-text float-start">Order from your favourite stores</p>
+                <img src="./img/groceries.png" class="float-end" alt="Groceries">
+              </div>
+            </div>
+          </div>
 
-  <!-- Serivce cards -->
-  <div class="mb-2 pt-5 mt-0">
-    <div class="container-fluid">
-      <div class="row g-2 justify-content-between">
-        <div class=" col-6 col-md-4">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">Rides</h5>
-              <p class="card-text float-start"> Transportation service that allows passengers
-                to request a ride from a location using a mobile app.</p>
-              <img src="./sabimages/Rectangle 17.png" class="float-end" alt="Rides">
-            </div>
-          </div>
-        </div>
-        <div class="col-6 col-md-4">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">VIP Rides</h5>
-              <p class="card-text float-start "> This service is designed to provide a higher level of comfort, luxury,
-                and personalized experience to users.</p>
-              <img src="./sabimages/Rectangle 31.png" class="" alt="VIP">
-            </div>
-          </div>
-        </div>
-        <div class="col-6 col-md-4">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">Food Delivery</h5>
-              <p class="card-text float-start">This is a service of bringing prepared meals directly to a customer's specified location.</p>
-              <img src="./sabimages/Rectangle 18.png" class="float-end" alt="Food delivery">
-            </div>
-          </div>
-        </div>
-        <div class="col-6 col-md-4">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">Groceries</h5>
-              <p class="card-text float-start">This is a service of bringing
-                groceries directly to a customer's
-                specified location.</p>
-              <img src="./sabimages/Rectangle 19.png" class="float-end" alt="Groceries">
-            </div>
-          </div>
-        </div>
-        <div class="col-6 col-md-4">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">Logistics</h5>
-              <p class="card-text float-start">This is a service of delivering
-                items directly to a customer's
-                specified location.</p>
-              <img src="./sabimages/Rectangle 28.png" class="float-end" alt="Logistics">
-            </div>
-          </div>
-        </div>
-        <div class="col-6 col-md-4">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">Dispatch</h5>
-              <p class="card-text float-start">This is a service of assigning and
-                coordinating the pickup and delivery of small goods. </p>
-              <img src="./sabimages/dispatch.png" class="float-end" width="120px" alt="Dispatch">
-            </div>
-          </div>
-        </div>
 
+
+        </div>
       </div>
     </div>
   </div>
@@ -319,8 +294,8 @@
       </div> -->
 
   <!-- News and update -->
-  <div class="container CTAFOUR my-3">
-    <div class="row News border rounded">
+  <div class="container CTAFOUR my-3 news">
+    <div class="row">
       <div class="col-lg-6 col-md-6 col-sm-6">
         <h1 class="fw-bolder text-black">News and Updates</h1>
       </div>
