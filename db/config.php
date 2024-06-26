dd29bfc18448a8441f724c0b1378b7f48268a85f
Dr milli <ekeminieffiong115@gmail.com>
2024-06-26 07:08:05 -0700

    db config

--- db/config.php
+++ db/config.php
@@ -1,10 +1,10 @@
 <?php
 // Database connection parameters
-$host = 'localhost';          // e.g., 'localhost' or '127.0.0.1'
+$host = 'c3nv2ev86aje4j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com';          // e.g., 'localhost' or '127.0.0.1'
 $port = '5432';          // default PostgreSQL port is 5432
-$dbname = 'sabi_ride';    // name of your database
-$user = 'postgres';      // database username
-$password = '1234';  // database password 
+$dbname = 'd72fknmleq0oq4';    // name of your database
+$user = 'usof740a83k29';      // database username
+$password = 'p6d372150f295f6b0f0377aeb7b9a628368a55f10aaa07c3d116484a05b0e20b8';  // database password 
 
 try {
     // Create a new PDO instance
@@ -14,7 +14,7 @@ try {
     // Set error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-   // echo "Connected to the PostgreSQL database successfully!";
+    echo "Connected to the PostgreSQL database successfully!";
 } catch (PDOException $e) {
     // Handle connection error
     echo "Connection failed: " . $e->getMessage();
