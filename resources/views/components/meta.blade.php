13adfee90a3d039e8701d7dc3e4ce8867597a6bf
hen8y <dheryhenry@gmail.com>
2024-08-03 13:08:44 +0100

    Meta Fix

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -3,4 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="theme-color" content="white">
+<meta name="description" content="Sabi Ride is a social ride hailing/sharing platform revolved around providing convenience, safety, and efficiency for riders and drivers.">
+<meta name="author" content="Sabi Ride">
+<link rel="icon" type="image/x-icon" href="/images/logo.png">
 {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
