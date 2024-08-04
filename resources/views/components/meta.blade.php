f80590df205bda03f0ee7e8925089e8d0d8685e8
hen8y <dheryhenry@gmail.com>
2024-08-04 21:01:49 +0100

    Meta Update

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -5,5 +5,17 @@
 <meta name="theme-color" content="white">
 <meta name="description" content="Sabi Ride is a social ride hailing/sharing platform revolved around providing convenience, safety, and efficiency for riders and drivers.">
 <meta name="author" content="Sabi Ride">
+<meta name="keyword" content="Sabi, Sabi ride, Ride Hailing, Ride sharing, Rides, Car pooling, Car sharing, Cab, Taxi, Ride">
 <link rel="icon" type="image/x-icon" href="/images/logo.png">
 {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
+<meta property="og:type" content="website">
+<meta property="og:url" content="{{ url()->full() }}">
+<meta property="og:title" content="@yield("title", config("app.name"))">
+<meta property="og:locale" content="en_GB">
+<meta property="og:description" content="@yield("description", "Sabi Ride is a social ride hailing/sharing platform revolved around providing convenience, safety, and efficiency for riders and drivers")">
+<meta property="og:image" content="@yield("image", asset("images/og.jpg"))">
+<meta name="twitter:url" content="{{ url()->full() }}">
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:title" content="@yield("title", config("app.name"))">
+<meta property="twitter:image" content="@yield("image", asset("images/og.jpg"))">
+<meta property="twitter:description" content="@yield("description", "Sabi Ride is a social ride hailing/sharing platform revolved around providing convenience, safety, and efficiency for riders and drivers")">
